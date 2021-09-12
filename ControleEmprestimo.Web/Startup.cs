0b40a723ab56d798c0e3f341a3eb41cbc90d8a1f
FRANCISCO OLIVEIRA <fransoliveira@gmail.com>
2021-09-12 20:45:25 -0300

    Add startup UseBrowserLink e criação views

--- ControleEmprestimo.Web/Startup.cs
+++ ControleEmprestimo.Web/Startup.cs
@@ -32,6 +32,7 @@ namespace ControleEmprestimo.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
             }
             else
             {
