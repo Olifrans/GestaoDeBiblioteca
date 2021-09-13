cdb767ac5b69c7b669fd1ba8015612c764920334
FRANCISCO OLIVEIRA <fransoliveira@gmail.com>
2021-09-12 22:16:31 -0300

    Views de listar e criar clientes

---
+++ ControleEmprestimo.Data/Services/ClienteService.cs
@@ -0,0 +1,19 @@
+﻿using ControleEmprestimo.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleEmprestimo.Data.Services
+{
+    public class ClienteService
+    {
+        public RepositoryCliente nRepositoryCliente { get; set; }
+
+        public ClienteService()
+        {
+            nRepositoryCliente = new RepositoryCliente();
+        }
+    }
+}
--- ControleEmprestimo.Web/Controllers/ClienteController.cs
+++ ControleEmprestimo.Web/Controllers/ClienteController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ControleEmprestimo.Data.Models;
+using ControleEmprestimo.Data.Services;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +10,16 @@ namespace ControleEmprestimo.Web.Controllers
 {
     public class ClienteController : Controller
     {
+        private ClienteService nClienteService = new ClienteService();
+
         public IActionResult Index()
         {
+            List<Cliente> nListService = nClienteService.nRepositoryCliente.SelecionarTodos();
+            return View(nListService);
+        }
+
+        public IActionResult Create()        {
+           // List<Cliente> nListService = nClienteService.nRepositoryCliente.SelecionarTodos();
             return View();
         }
     }
